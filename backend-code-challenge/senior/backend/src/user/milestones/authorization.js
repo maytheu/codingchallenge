d7874ae8e4c1bc8de7e200cf73d5027727ed7f45
maytheu <maytheu98@gmail.com>
2024-05-27 08:46:34 +0100

    authorization

--- backend-code-challenge/senior/backend/src/user/milestones/authorization.js
+++ backend-code-challenge/senior/backend/src/user/milestones/authorization.js
@@ -1,16 +1,18 @@
 'use strict';
 
-// const {
-//   Errors: { ForbiddenError },
-// } = require('finale-rest');
+const {
+  Errors: { ForbiddenError },
+} = require('finale-rest');
 
-// const {
-//   API: {
-//     KEY,
-//     HEADERS: { X_API_KEY },
-//   },
-// } = require('../../config');
+const {
+  API: {
+    KEY,
+    HEADERS: { X_API_KEY },
+  },
+} = require('../../config');
 
 module.exports = (req, res, context) => {
+  const key = req.header(X_API_KEY)
+  if(!key) throw new ForbiddenError('Wrong API key')
   return context.continue;
 };
--- backend-code-challenge/senior/backend/src/user/milestones/authorization.spec.js
+++ backend-code-challenge/senior/backend/src/user/milestones/authorization.spec.js
@@ -17,7 +17,7 @@ const {
   },
 } = require('../../config');
 
-describe.skip('Authorization Milestone', () => {
+describe('Authorization Milestone', () => {
   const mockHeader = jest.fn();
 
   let req = null;
