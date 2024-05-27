22ebbff8b3cba529d18a0bb6cc0a4fee8e9346bd
maytheu <maytheu98@gmail.com>
2024-05-27 08:38:36 +0100

    authenticate

--- backend-code-challenge/senior/backend/src/user/milestones/authentication.js
+++ backend-code-challenge/senior/backend/src/user/milestones/authentication.js
@@ -1,16 +1,20 @@
 'use strict';
 
-// const {
-//   Errors: { BadRequestError },
-// } = require('finale-rest');
+const {
+  Errors: { BadRequestError },
+} = require('finale-rest');
 
-// const {
-//   API: {
-//     HEADERS: { X_SLUG },
-//     SLUGS: { MYSELF },
-//   },
-// } = require('../../config');
+const {
+  API: {
+    HEADERS: { X_SLUG },
+    SLUGS: { MYSELF },
+  },
+} = require('../../config');
 
 module.exports = (req, res, context) => {
+  const slug = req.header(X_SLUG) ;
+  if (!slug) throw new BadRequestError('Missing user slug');
+  const user = Buffer.from(slug, 'base64').toString('utf-8')
+  req.params.slug = user
   return context.continue;
 };
